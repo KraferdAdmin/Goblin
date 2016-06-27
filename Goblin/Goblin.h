77faf352bed330e257e5d27ab3afbcf436b8019b
Strat Aguilar <saguilar@Corp-iMac30.local>
2016-06-27 11:11:09 -0700

    commit"

---
+++ Goblin/Goblin.h
@@ -0,0 +1,19 @@
+//
+//  Goblin.h
+//  Goblin
+//
+//  Created by Strat Aguilar on 6/27/16.
+//  Copyright © 2016 Kraferd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Goblin.
+FOUNDATION_EXPORT double GoblinVersionNumber;
+
+//! Project version string for Goblin.
+FOUNDATION_EXPORT const unsigned char GoblinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Goblin/PublicHeader.h>
+
+
---
+++ Goblin/Goblin.swift
@@ -0,0 +1,20 @@
+//
+//  Goblin.swift
+//  Goblin
+//
+//  Created by Strat Aguilar on 6/27/16.
+//  Copyright © 2016 Kraferd. All rights reserved.
+//
+
+import Foundation
+
+
+public struct Goblin{
+    public func getGold(){
+        print("Money Money Money")
+    }
+    
+    public init(){
+        
+    }
+}
